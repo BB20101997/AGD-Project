c359c91ed28690f5608d7da5870f4de7f91abbcb
Sloner42 <sloner@web.de>
2018-06-06 15:47:27 +0200

    Begin Model with code from old project

---
+++ model/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Created by BB20101997 on 31. Mai. 2018.
+ */
+module de.webtwob.agd.project.control {
+	
+    requires org.eclipse.elk.graph;
+    
+    
+    
+
+    exports de.webtwob.agd.project.model;
+}
